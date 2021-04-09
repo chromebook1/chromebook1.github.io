7d845a5c8d45bb495485050c1889dc11452d3928
lazyandsleepy <>
2021-04-09 19:58:13 +0000

    commit

---
+++ commit.sh
@@ -0,0 +1,8 @@
+git init
+git add --all
+git status
+git config --global user.name "lazyandsleepy"
+git config --global user.email "IABademos@cps.edu"
+git commit -m "commit"
+git remote add origin https://github.com/chromebook1/chromebook1.github.io.git.git
+git push -u origin master
